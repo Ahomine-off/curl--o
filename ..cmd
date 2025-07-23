8c4c84936fbc84413b57c72cd825daed09e33ab7
Ahomine-off <taryti577@gmail.com>
2025-07-23 19:54:03 +0000

    new file:   ..cmd

---
+++ ..cmd
@@ -0,0 +1,8 @@
+@echo off
+cd \
+cd patoune
+echo your computer a été miauler par cat test>cat.txt
+start notepad cat.txt >nul
+timeout 2 >nul
+start https://img.freepik.com/photos-premium/mignon-petit-chaton-britannique-bleu-gris-dormant-enveloppe-dans-couverture-carreaux-blanche-concept-adorables-animaux-compagnie_106368-4291.jpg?w=2000 >nul
+exit /b 0
